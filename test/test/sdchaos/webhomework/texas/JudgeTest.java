ac7f2d6ce927db6aeb4a6c75b5fb993ba19076c7
SDchao <wentaochens@outlook.com>
2020-06-23 19:40:06 +0800

    Updated Test

--- test/test/sdchaos/webhomework/texas/JudgeTest.java
+++ test/test/sdchaos/webhomework/texas/JudgeTest.java
@@ -33,6 +33,7 @@ public void after() {
 */ 
 @Test
 public void testCompare() {
+    // 1
     Card[] player1 = {
             new Card("2","H"),
             new Card("3","D"),
@@ -51,6 +52,66 @@ public void testCompare() {
 
     int r = judge.Compare(player1,player2);
     Assert.assertEquals(r, Judge.PLAYER2_WIN);
+
+    // 2
+    player1 = new Card[] {
+            new Card("2", "H"),
+            new Card("4", "S"),
+            new Card("4", "C"),
+            new Card("2", "D"),
+            new Card("4", "H")
+    };
+
+    player2 = new Card[] {
+            new Card("2", "S"),
+            new Card("8", "S"),
+            new Card("A", "S"),
+            new Card("Q", "S"),
+            new Card("3", "S")
+    };
+
+    r = judge.Compare(player1,player2);
+    Assert.assertEquals(r, Judge.PLAYER2_WIN);
+
+    // 3
+    player1 = new Card[] {
+            new Card("2", "H"),
+            new Card("3", "H"),
+            new Card("5", "H"),
+            new Card("9", "H"),
+            new Card("K", "H")
+    };
+
+    player2 = new Card[] {
+            new Card("2", "C"),
+            new Card("3", "H"),
+            new Card("4", "S"),
+            new Card("5", "C"),
+            new Card("6", "H")
+    };
+
+    r = judge.Compare(player1,player2);
+    Assert.assertEquals(r, Judge.PLAYER1_WIN);
+
+    // 4
+    player1 = new Card[] {
+            new Card("2", "H"),
+            new Card("3", "D"),
+            new Card("5", "S"),
+            new Card("9", "C"),
+            new Card("K", "D")
+    };
+
+    player2 = new Card[] {
+            new Card("2", "D"),
+            new Card("3", "H"),
+            new Card("5", "C"),
+            new Card("K", "H"),
+            new Card("9", "S")
+    };
+
+    r = judge.Compare(player1,player2);
+    Assert.assertEquals(r, Judge.TIE);
 } 
 
 
